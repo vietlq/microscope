7bd9161f00ba62184a5e9691c8c163410d483ee3
Viet Le <vietlq85@gmail.com>
2016-04-28 21:10:53 +0100

    Added routing & error handling

--- client/main.js
+++ client/main.js
@@ -1,6 +1,2 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Meteor.subscribe('posts');
+//import { Template } from 'meteor/templating';
+//import { ReactiveVar } from 'meteor/reactive-var';
--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,7 +1,10 @@
 Template.postItem.helpers({
+    id() {
+        return this._id.valueOf();
+    },
     domain() {
         let a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
-    }
+    },
 });
