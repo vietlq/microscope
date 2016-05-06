79546d81282cac60444d7b93ab2186177ef83acd
Viet Le <vietlq85@gmail.com>
2016-05-06 21:47:37 +0100

    Added loading of single posts for discussion & editing

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,11 @@ Meteor.publish('posts', function(options) {
     return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(_id) {
+    check(_id, String);
+    return Posts.find({_id});
+});
+
 Meteor.publish('comments', function(postId) {
     check(postId, String);
 
