961f8453f58c70726f7614d6d9b7f99d37e4e9c2
Viet Le <vietlq85@gmail.com>
2016-05-07 22:17:30 +0100

    Added viet:errors

---
+++ packages/errors/package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: "viet:errors",
+  summary: "A pattern to display application errors to the user",
+  version: "0.0.2"
+});
+
+Package.onUse(function (api, where) {
+  api.versionsFrom('0.9.0');
+
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+
+  if (api.export)
+    api.export('Errors');
+});
+
+Package.onTest(function(api) {
+  api.use('viet:errors', 'client');
+  api.use(['tinytest', 'templating', 'test-helpers'], 'client');
+
+  api.addFiles('errors_tests.js', 'client');
+});
