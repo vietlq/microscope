4a9afdd0041a2abac73bff0f52d0d1545cdd140b
Viet Le <vietlq85@gmail.com>
2016-05-07 09:31:09 +0100

    Added upvote feature

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -13,4 +13,22 @@ Template.postItem.helpers({
     time_ago() {
         return moment(this.submitted).fromNow();
     },
+    upvotedClass() {
+        const userId = Meteor.userId();
+        // If the userId is not among upvoters, still can vote
+        if (userId && !_.include(this.upvoters, userId)) {
+            return 'btn-primary upvotable';
+        } else {
+            return 'disabled';
+        }
+    },
+});
+
+Template.postItem.events({
+    // Only works for .upvotable classes
+    'click .upvotable'(e) {
+        e.preventDefault();
+
+        Meteor.call('upvote', this._id);
+    },
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -24,7 +24,10 @@ if (Posts.find().count() === 0) {
     userId: misha._id,
     author: misha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
-    submitted: new Date(now - 7 * 3600 * 1000)
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0,
   });
 
   Comments.insert({
@@ -48,7 +51,10 @@ if (Posts.find().count() === 0) {
     userId: viet._id,
     author: viet.profile.name,
     url: 'http://meteor.com',
-    submitted: new Date(now - 10 * 3600 * 1000)
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0,
   });
 
   Posts.insert({
@@ -56,6 +62,9 @@ if (Posts.find().count() === 0) {
     userId: viet._id,
     author: viet.profile.name,
     url: 'http://themeteorbook.com',
-    submitted: new Date(now - 12 * 3600 * 1000)
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0,
   });
 }
